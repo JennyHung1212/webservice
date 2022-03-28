250af880af88e86521c7faf4fc59f077ca1597fd
Jenny Hung <jennyhung1212@gmail.com>
2022-03-28 00:35:13 -0400

    modify codeploy.sh

--- ami/codedeploy.sh
+++ ami/codedeploy.sh
@@ -5,6 +5,6 @@ $CODEDEPLOY_BIN stop
 yum erase codedeploy-agent -y
 cd /home/ec2-user
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
-chmod +x ./install
+sudo chmod +x ./install
 sudo ./install auto
-echo $(sudo service codedeploy-agent stat
+echo $(sudo service codedeploy-agent status)
