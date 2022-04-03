24d0897f20441a7e15509ea51f6c83d6fd1616c7
Jenny Hung <jennyhung1212@gmail.com>
2022-04-03 13:07:06 -0400

    add statsd bean

--- src/main/java/edu/neu/coe/csye6225/webapp/repositories/UserRepository.java
+++ src/main/java/edu/neu/coe/csye6225/webapp/repositories/UserRepository.java
@@ -1,8 +1,12 @@
 package edu.neu.coe.csye6225.webapp.repositories;
 
 import edu.neu.coe.csye6225.webapp.models.User;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Qualifier("user")
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
     User findByUsername(String username);
 }
